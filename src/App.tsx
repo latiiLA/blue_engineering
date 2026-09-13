eee7b8d2929d9ace1d0fb3b709cbcc1a18a94039
Lata Amenu <amelatbay@gmail.com>
2026-09-13 23:01:11 +0300

    global updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import "./App.css";
-import Header from "./pages/Header";
-import Navbar from "./pages/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "@/components/layout/Layout";
+import HomePage from "@/pages/HomePage";
+import AboutPage from "@/pages/AboutPage";
+import OrganizationPage from "@/pages/OrganizationPage";
+import TeamPage from "@/pages/TeamPage";
+import ServicesPage from "@/pages/ServicesPage";
+import ServiceCategoriesPage from "@/pages/ServiceCategoriesPage";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Navbar />
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="about/organization" element={<OrganizationPage />} />
+        <Route path="about/team" element={<TeamPage />} />
+        <Route path="services" element={<ServicesPage />} />
+        <Route path="services/categories" element={<ServiceCategoriesPage />} />
+      </Route>
+    </Routes>
   );
 }
 
